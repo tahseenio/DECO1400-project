c9c0960b19a921e29e34ad485e3ecbaa84053550
Tahseen <tahseenislam@hotmail.com>
2022-05-17 15:45:36 +1000

    refactored contact page code

--- js/contact.js
+++ js/contact.js
@@ -5,27 +5,12 @@ const lastName = document.getElementById('lastName')
 const email = document.getElementById('email')
 const message = document.getElementById('message')
 
-let firstNameValue = null
-let lastNameValue = null
-let emailValue = null
-let messageValue = null
-
-
 const handleFormSubmit = () => {
-  alert(`Submitted. First Name: ${firstNameValue}, Last Name: ${lastNameValue}, Email: ${emailValue}, Message: ${messageValue}`)
+  alert(`Submitted. First Name: ${firstName.value}, Last Name: ${lastName.value}, Email: ${email.value}, Message: ${message.value}`)
 }
 
+const form = document.querySelector('.contact-form--right')
 
-const handleChange = (value) => {
-  if (value === 'firstName') {
-    firstNameValue = document.getElementById('firstName').value
-  } else if (value === 'lastName') {
-    lastNameValue = document.getElementById('lastName').value
-  } else if (value === 'email') {
-    emailValue = document.getElementById('email').value
-  } else if (value === 'message') {
-    messageValue = document.getElementById('message').value
-  }
-}
+form.addEventListener('submit', handleFormSubmit)
 
 
