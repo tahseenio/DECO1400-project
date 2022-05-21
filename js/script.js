72133a0339a69c1b0a40687da24e2c144c0d8ae4
Tahseen <tahseenislam@hotmail.com>
2022-05-21 20:19:34 +1000

    added slideshow

--- js/script.js
+++ js/script.js
@@ -31,3 +31,40 @@ const socalTacosDetailBtn = document.getElementById('socal-tacos');
 socalTacosDetailBtn.addEventListener('click', () => {
   localStorage.setItem('id', '36206');
 });
+
+// Slideshow
+let slideIndex = 0;
+const slideShowDelay = 10;
+
+const slides = document.querySelectorAll('.slide');
+const carouselBackBtn = document.querySelector('.carousel__left');
+const carouselForwardBtn = document.querySelector('.carousel__right');
+const carouselPills = document.querySelectorAll('.circle');
+
+const ShowSlide = (index) => {
+  if (index === slides.length) {
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+    carouselPills[i].classList.remove('active-pill');
+  }
+
+  slides[slideIndex].style.display = 'block';
+  carouselPills[slideIndex].classList.add('active-pill');
+};
+
+carouselBackBtn.addEventListener('click', () => {
+  ShowSlide(--slideIndex);
+});
+
+carouselForwardBtn.addEventListener('click', () => {
+  ShowSlide(++slideIndex);
+});
+
+carouselPills.forEach((pill) => pill.addEventListener('click', (e) => {}));
+
+ShowSlide(slideIndex);
